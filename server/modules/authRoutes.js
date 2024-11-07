5caa4bce1d20e02e2e7160f6e248a4ddac2dcc28
Rupam Das <somadaslove9876@gmail.com>
2024-11-07 13:40:29 +0530

    initial :)

--- server/modules/authRoutes.js
+++ server/modules/authRoutes.js
@@ -24,7 +24,7 @@ router.get("/login/failed", (req, res) => {
 router.get("/google", passport.authenticate("google", ["profile", "email"]));
 
 router.get(
-  "/google/callback", 
+  "https://codetit-api.vercel.app/google/callback", 
   passport.authenticate('google', { failureRedirect: '/auth/login/failed' }), 
   async function (req, res, next) {
     const profile = req.user;  // Profile data will now be available here
