8cd7068d1fda71c9ae445cf9291474f3fbaf6e42
Rupam Das <somadaslove9876@gmail.com>
2024-11-18 20:59:23 +0530

    gpt

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom'; // Use ReactDOM instead of createRoot
+import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routers from './Routers'; // Import the Routers component
 import { UserProvider } from './context/user';
-import { StructureProvider } from './context/structure'
-import { Toaster } from "@/components/ui/toaster"
+import { StructureProvider } from './context/structure';
+import { Toaster } from "@/components/ui/toaster";
 
-createRoot(document.getElementById('root')).render(
+// Use ReactDOM.render instead of createRoot
+ReactDOM.render(
   <StrictMode>
     <UserProvider>
-    <StructureProvider>
-      <Router>
-        <Routers />
-        <Toaster />
-      </Router>
-    </StructureProvider>
+      <StructureProvider>
+        <Router>
+          <Routers />
+          <Toaster />
+        </Router>
+      </StructureProvider>
     </UserProvider>
   </StrictMode>,
-)
+  document.getElementById('root')
+);
